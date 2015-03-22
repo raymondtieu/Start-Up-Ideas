17b0875084be1a17be9cc116754a0ae8089ea41b
Raymond <iamraymondtieu@gmail.com>
2015-03-22 18:01:37 -0400

    need to confirm idea delete

--- public/scripts/startup.js
+++ public/scripts/startup.js
@@ -1,4 +1,4 @@
-var app = angular.module('startUp', ['ui.bootstrap', 'chart.js']);
+var app = angular.module('startUp', ['ui.bootstrap', 'chart.js', 'ngBootbox']);
 
 /* Functions used across multiple controllers */
 
